66b5e3b62ca9192e850fd9f34c18912a016241d1
EvgenyTreskin <treskinei@gmail.com>
2024-09-19 07:41:59 +0300

    Homework 1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
@@ -2,17 +2,33 @@ package guru.qa.niffler.page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class LoginPage {
-  private final SelenideElement usernameInput = $("input[name='username']");
-  private final SelenideElement passwordInput = $("input[name='password']");
-  private final SelenideElement submitButton = $("button[type='submit']");
+    private final SelenideElement usernameInput = $("input[name='username']");
+    private final SelenideElement passwordInput = $("input[name='password']");
+    private final SelenideElement submitButton = $("button[type='submit']");
+    private final SelenideElement newAccountButton = $("a.form__register");
+    private final SelenideElement badCredentialError = $("p.form__error");
 
-  public MainPage login(String username, String password) {
-    usernameInput.setValue(username);
-    passwordInput.setValue(password);
-    submitButton.click();
-    return new MainPage();
-  }
+    public MainPage login(String username, String password) {
+        usernameInput.setValue(username);
+        passwordInput.setValue(password);
+        submitButton.click();
+        return new MainPage();
+    }
+
+    public void unSucceedLogin(String username, String password) {
+        usernameInput.setValue(username);
+        passwordInput.setValue(password);
+        submitButton.click();
+        badCredentialError.should(appear);
+    }
+
+    public SignUpPage createAccount() {
+        newAccountButton.click();
+        return new SignUpPage();
+    }
 }
---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -0,0 +1,33 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.BrowserExtension;
+import guru.qa.niffler.page.LoginPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(BrowserExtension.class)
+public class LoginTest {
+
+    private static final Config CFG = Config.getInstance();
+
+    @Test
+    void mainPageShouldBeDisplayedAfterSuccessLogin() {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login("duck", "12345")
+                .checkStatisticAndHistoryOfSpendingAppear();
+    }
+
+    @Test
+    void userShouldStayOnLoginPageAfterLoginWithBadCredentialLoginAndShowError() {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .unSucceedLogin("duck", "qwerty");
+    }
+
+    @Test
+    void userShouldStayOnLoginPageAfterLoginWithBadCredentialPasswordAndShowError() {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .unSucceedLogin("duckyyy", "12345");
+    }
+}
