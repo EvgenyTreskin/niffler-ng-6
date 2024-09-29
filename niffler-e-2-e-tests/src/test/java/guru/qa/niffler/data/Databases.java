067d072d31874c9e95458542e694088ce7f49475
EvgenyTreskin <treskinei@gmail.com>
2024-09-29 14:19:09 +0300

    lesson4.2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/Databases.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/Databases.java
@@ -5,6 +5,7 @@ import org.postgresql.ds.PGSimpleDataSource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -13,6 +14,7 @@ public class Databases {
     }
 
     private static final Map<String, DataSource> dataSources = new ConcurrentHashMap<>();
+    private static final Map<Long, Map<String, Connection>> threadConnections = new ConcurrentHashMap<>();
 
     private static DataSource dataSource(String jdbcUrl) {
         return dataSources.computeIfAbsent(
@@ -27,7 +29,43 @@ public class Databases {
         );
     }
 
-    public static Connection connection(String jdbcUrl) throws SQLException {
-        return dataSource(jdbcUrl).getConnection();
+    private static Connection connection(String jdbcUrl) throws SQLException {
+        return threadConnections.computeIfAbsent(
+                Thread.currentThread().threadId(),
+                key -> {
+                    try {
+                        return new HashMap<>(Map.of(
+                                jdbcUrl,
+                                dataSource(jdbcUrl).getConnection()
+                        ));
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        ).computeIfAbsent(
+                jdbcUrl,
+                key -> {
+                    try {
+                        return dataSource(jdbcUrl).getConnection();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+    }
+
+    public static void closeAllConnections() {
+        for (Map<String, Connection> connectionMap : threadConnections.values()) {
+            for (Connection connection : connectionMap.values()) {
+                try {
+                    if (connection != null && !connection.isClosed()) {
+                        connection.close();
+                    }
+                } catch (SQLException e) {
+//                   не выбрасывать исключения, чтоб в случае ошибки в одном из коннекшенов
+//                   продолжали закрываться другие коннекшены
+                }
+            }
+        }
     }
 }
